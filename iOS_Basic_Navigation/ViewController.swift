96e4fcd08a2795b06335afca4afa0aa60c87672d
Brian Bansenauer <brianb@cascadia.edu>
2019-06-20 11:01:12 -0700

    Initial Commit

---
+++ iOS_Basic_Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Basic_Navigation
+//
+//  Created by Brian Bansenauer on 6/20/19.
+//  Copyright © 2019 Brian Bansenauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
